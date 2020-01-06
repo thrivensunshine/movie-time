9766bc705e5a367beec189d3fa39e27f65b6daee
Danielle Alba <danib.sgh@gmail.com>
2020-01-06 08:19:46 -0500

    from config imported to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import HeroImage from './elements/HeroImage'
-import SearchBar from './elements/SearchBar'
-import Grid from './elements/Grid'
-import MovieThumb from './elements/MovieThumb'
-import LoadMoreBtn from './elements/LoadMoreBtn'
-import Spinner from './elements/Spinner'
+import {API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../config';
+import HeroImage from './elements/HeroImage';
+import SearchBar from './elements/SearchBar';
+import Grid from './elements/Grid';
+import MovieThumb from './elements/MovieThumb';
+import LoadMoreBtn from './elements/LoadMoreBtn';
+import Spinner from './elements/Spinner';
 
 const Home = () => {
 return (
@@ -16,7 +17,7 @@ return (
     <Spinner />
     <LoadMoreBtn />
   </>
-  
+
 
 )
 
