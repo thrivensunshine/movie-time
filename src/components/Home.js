a172e5ca9a6f2ef47af0ebeb7936688f0dbb67a0
Danielle Alba <danib.sgh@gmail.com>
2020-01-07 11:46:57 -0500

    fetch Movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,45 @@ import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
 
 const Home = () => {
-  const [state, setState] = useState({movies: []});
+  const [state, setState] = useState({movies: [] });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false)
 
-  console.log(state + " state in home.js")
+  //console.log(state + " COME ON MAN")
+
+  //asynch
+  const fetchMovies = async endpoint => {
+    setError(false);
+    setLoading(true);
+
+    //run this code first
+     try{
+       const result = await (await fetch(endpoint)).json();
+       console.log(result )
+       setState(prev => ({
+         ...prev,
+         movies: [...result.results],
+         heroImage: prev.heroImage || result.results[0],
+         currentPage: result.page,
+         totalPages: result.total_pages
+
+       }));
+
+     }
+     //if try doesnt work run the catch
+     catch(error){
+       setError(true)
+       console.log(error)
+     }
+     //not longer loading
+     setLoading(false)
+   }
+
+     useEffect( () => {
+       fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+     }, [fetchMovies])
+
+     console.log(state + " state in home.js")
 
 return (
   <>
@@ -28,3 +62,8 @@ return (
 }
 
 export default Home;
+
+
+///////////////////////////////////////////////////////
+
+//*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
--- src/config.js
+++ src/config.js
@@ -3,7 +3,12 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
+// from the video key
 const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+
+
+
+
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
